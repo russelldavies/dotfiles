336fa3e39718c71db343561c7301c1cdd3162ac4
Russell Davies <russell@zeroflux.net>
2014-12-09 22:24:34 +0000

    Update 10_osx.sh
    Cleaned up and tweaked brew package list and python packages.

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -1,77 +1,42 @@
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
-homebrew='https://raw.github.com/mxcl/homebrew/go'
-
 # Some tools look for XCode, even though they don't need it.
 # https://github.com/joyent/node/issues/3681
 # https://github.com/mxcl/homebrew/issues/10245
-if [[ ! -d "$('xcode-select' -print-path 2>/dev/null)" ]]; then
-    sudo xcode-select -switch /usr/bin
-fi
-
-# Install Homebrew.
-if [[ ! "$(type -P brew)" ]]; then
-    e_header "Installing Homebrew"
-    true | /usr/bin/ruby -e "$(/usr/bin/curl -fsSL $homebrew)"
-fi
+# TODO: remove if not an issue anymore
+#if [[ ! -d "$('xcode-select' -print-path 2>/dev/null)" ]]; then
+#    sudo xcode-select -switch /usr/bin
+#fi
 
 if [[ "$(type -P brew)" ]]; then
-    # Make sure we’re using the latest Homebrew
     brew update
-    # Upgrade any already-installed formulae
     brew upgrade
 
     e_header "Install packages"
-    # Install GNU core utilities (those that come with OS X are outdated)
-    brew install coreutils
-    echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
-    # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-    brew install findutils
-    # Install Bash 4
-    brew install bash bash-completion
-
-    # Install wget with IRI support
-    brew install wget --enable-iri
-
-    # Install more recent versions of some OS X tools
+    # GNU core utilities (those that come with OS X are outdated)
     brew tap homebrew/dupes
-    brew install homebrew/dupes/grep
+    brew install coreutils findutils grep bash bash-completion
 
-    # Tools
+    # Tools and Utils
+    brew install git git-extras
     brew install tmux
-    brew install ack
-    brew install git
-    brew install git-extras
-    brew install tree
-    brew install lesspipe
-    brew install htop-osx
+    brew install ack tree lesspipe htop-osx
     brew install reattach-to-user-namespace
+    brew install python
 
-    # Python
-    brew install python --framework
-
-    if [[ ! "$(type -P gcc-4.2)" ]]; then
-        e_header "Installing Homebrew dupe recipe: apple-gcc42"
-        brew install https://raw.github.com/Homebrew/homebrew-dupes/master/apple-gcc42.rb
-    fi
+    # TODO: remove if not needed anymore
+    #if [[ ! "$(type -P gcc-4.2)" ]]; then
+    #    e_header "Installing Homebrew dupe recipe: apple-gcc42"
+    #    brew install https://raw.github.com/Homebrew/homebrew-dupes/master/apple-gcc42.rb
+    #fi
 
     # Remove outdated versions from the cellar
     brew cleanup
 fi
 
-# Python dev
-[[ $(type -P pip) ]] || sudo easy_install pip
-
-packages=(
-	virtualenv virtualenvwrapper
-)
-list=()
-for package in "${packages[@]}"; do
-    # If package is not installed add to list
-    [[ $(pip show "$package") ]] || list=("${list[@]}" "$package")
-done
-if (( ${#list[@]} > 0 )); then
-    e_header "Installing pip packages: ${list[*]}"
-    sudo pip -q install -U ${list[*]}
-fi
+# Python tasks
+e_header "Upgrading Python sys packages"
+pip install --upgrade setuptools
+pip install --upgrade pip
+pip install --upgrade virtualenv virtualenvwrapper
