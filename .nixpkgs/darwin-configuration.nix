87d171f3344d8074b466d236538b15f46fe0daf7
Russell Davies <russell@zeroflux.net>
2021-11-01 17:48:33 +0100

    Add nixpkgs

---
+++ .nixpkgs/darwin-configuration.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+      aws-vault
+      awscli2
+      coreutils
+      curlie
+      direnv
+      editorconfig-core-c
+      entr
+      fx
+      fzf
+      jq
+      kakoune
+      nmap
+      noti
+      pinentry_mac
+      postgresql_13
+      pup
+      pwgen
+      rage
+      socat
+      sqlite
+      tor
+      torsocks
+      yubikey-agent
+      #zsh
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  # services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
